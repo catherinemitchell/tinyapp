b4db82061135694a5e5cc998aea3a0eebd9083a3
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-20 21:20:15 -0300

    implemented the function generateRandomString(), but left empty for now

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -47,3 +49,4 @@ app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
+
